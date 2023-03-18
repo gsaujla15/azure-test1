7b9d70ee0406b913be91892ae1300b2d48a14707
arunkumar-madhavan <arunkmadhavan88@gmail.com>
2023-03-18 17:59:07 -0400

    AKM::Added changes for new DB

--- src/main/java/LoginDao.java
+++ src/main/java/LoginDao.java
@@ -11,142 +11,153 @@ public class LoginDao {
 	private static Connection connection = null;
 
 	public static boolean validate(String name, String pass) {
-        boolean status = false;
-        Connection conn = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
+		boolean status = false;
+		Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
 
-        String url = "jdbc:mysql://localhost/";
-        String dbName = "COMPOSITEAPPS";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "mysqluser";
-        String password = "mysqlpassword";
+		String url = "jdbc:mysql://loyalist-internship-demo-db.mysql.database.azure.com:3306/";
+		String dbName = "COMPOSITEAPPS?useSSL=true";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String userName = "loyalist";
+		String password = "*TeamB1*";
+		String urlFinal = url + dbName;
+		System.out.println(urlFinal);
 
-        try {
-            Class.forName(driver).newInstance();
-            conn = DriverManager.getConnection(url + dbName, userName, password);
+		try {
+			Class.forName(driver).newInstance();
+			conn = DriverManager.getConnection(url + dbName, userName, password);
+			System.out.println(url + dbName + userName + password);
 
-            pst = conn.prepareStatement("SELECT * FROM EMPLOYEES WHERE USERNAME=? and PASSWORD=?");
-            pst.setString(1, name);
-            pst.setString(2, pass);
+			pst = conn.prepareStatement("SELECT * FROM EMPLOYEES WHERE USERNAME=? and PASSWORD=?");
+			pst.setString(1, name);
+			pst.setString(2, pass);
 
-            rs = pst.executeQuery();
-            status = rs.next();
-        }
+			rs = pst.executeQuery();
+			status = rs.next();
+		}
 
-        catch (Exception e) {
-            System.out.println(e);
-        }
+		catch (Exception e) {
+			System.out.println(e);
+		}
 
-        finally {
-        	if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 
-            if (pst != null) {
-                try {
-                    pst.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+			if (pst != null) {
+				try {
+					pst.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-        return status;
-    }
+		return status;
+	}
 
 	// Create permanent connection
 	public static Connection getConnection() {
-        if (connection != null)
-            return connection;
-        else {
-            try {
+		if (connection != null)
+			return connection;
+		else {
+			try {
 
-            	String url = "jdbc:mysql://localhost/";
-                String dbName = "COMPOSITEAPPS";
-                String driver = "com.mysql.jdbc.Driver";
-                String userName = "mysqluser";
-                String password = "mysqlpassword";
+				String url = "jdbc:mysql://loyalist-internship-demo-db.mysql.database.azure.com:3306/";
+				String dbName = "COMPOSITEAPPS?useSSL=true";
+				String driver = "com.mysql.cj.jdbc.Driver";
+				String userName = "loyalist";
+				String password = "*TeamB1*";
+				String urlFinal = url + dbName;
+				System.out.println(url + dbName + userName + password);
+				Class.forName(driver);
 
-                Class.forName(driver);
-                connection = DriverManager.getConnection(url + dbName, userName, password);
+				connection = DriverManager.getConnection(urlFinal, userName, password);
 
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return connection;
-        }
-    }
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return connection;
+		}
+	}
 
 	public static ArrayList<Employee> getAllEmployees() {
 
-		 connection = LoginDao.getConnection();
-	     ArrayList<Employee> employeeList = new ArrayList<Employee>();
-	     Statement statement = null;
-	     ResultSet rs = null;
+		connection = LoginDao.getConnection();
+		ArrayList<Employee> employeeList = new ArrayList<Employee>();
+		Statement statement = null;
+		ResultSet rs = null;
 
-	        try {
-	            statement = connection.createStatement();
-	            rs = statement.executeQuery("SELECT * FROM EMPLOYEES LIMIT 100");
+		try {
+			statement = connection.createStatement();
+			rs = statement.executeQuery("SELECT * FROM EMPLOYEES LIMIT 100");
 
-	            while(rs.next()) {
-	            	Employee empl = new Employee();
-	                empl.setEmployeeID(rs.getInt("EMPLOYEE_ID"));
-	                empl.setName(rs.getString("NAME"));
-	                empl.setPhone(rs.getString("PHONE_NUMBER"));
-	                empl.setSupervisor(rs.getString("SUPERVISORS"));
-	                empl.setUsername(rs.getString("USERNAME"));
-	                empl.setPassword(rs.getString("PASSWORD"));
-	                employeeList.add(empl);
-	            }
-	        }
+			while (rs.next()) {
+				Employee empl = new Employee();
+				empl.setEmployeeID(rs.getInt("EMPLOYEE_ID"));
+				empl.setName(rs.getString("NAME"));
+				empl.setPhone(rs.getString("PHONE_NUMBER"));
+				empl.setSupervisor(rs.getString("SUPERVISORS"));
+				empl.setUsername(rs.getString("USERNAME"));
+				empl.setPassword(rs.getString("PASSWORD"));
+				employeeList.add(empl);
+			}
+		}
 
-	        catch (SQLException e) {
-	            e.printStackTrace();
-	        }
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-	        finally {
-	        	if (rs != null) {
-	                try {
-	                    rs.close();
-	                } catch (SQLException e) {
-	                    e.printStackTrace();
-	                }
-	            }
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 
-	            if (statement != null) {
-	                try {
-	                	statement.close();
-	                } catch (SQLException e) {
-	                	e.printStackTrace();
-	                }
-	            }
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 
-	        }
+		}
 
-	        return employeeList;
-	    }
+		return employeeList;
+	}
+
 	public static void closeConnection() {
 		if (connection != null) {
-            try {
-            	connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		LoginDao login = new LoginDao();
+		login.validate("driley0", "XBXA3XgR");
 	}
 }
