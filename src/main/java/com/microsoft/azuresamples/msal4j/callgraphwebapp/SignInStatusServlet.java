b39056a65917601baedffaa09c2741d732c259ed
arunkumar-madhavan <arunkmadhavan88@gmail.com>
2023-03-18 20:41:11 -0400

    AKM::Added changes for new DB

--- src/main/java/com/microsoft/azuresamples/msal4j/callgraphwebapp/SignInStatusServlet.java
+++ src/main/java/com/microsoft/azuresamples/msal4j/callgraphwebapp/SignInStatusServlet.java
@@ -21,7 +21,7 @@ public class SignInStatusServlet extends HttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("bodyContent", "content/status.jsp");
+        req.setAttribute("bodyContent", "login.jsp");
         final RequestDispatcher view = req.getRequestDispatcher("index.jsp");
         view.forward(req, resp);
     }
